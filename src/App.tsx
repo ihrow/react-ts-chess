f9cff66ebd0b7a3e1fd31b65ce2a0aea15b45755
igor <igornotfound@gmail.com>
2023-03-17 23:54:30 +0100

    board & styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import "./App.css";
+import BoardComponent from "./components/BoardComponent";
+import {Board} from "./models/Board";
 
 const App = () => {
+  const [board, setBoard] = useState<Board>(new Board());
+
+  useEffect(() => {
+    restart();
+  }, [])
+
+  const restart = () => {
+    const newBoard = new Board();
+    newBoard.initCells();
+    newBoard.addFigures();
+    setBoard(newBoard)
+  }
+
+
   return (
-    <div>
-      
+    <div className="app">
+      <div className="board">
+        <BoardComponent board={board} setBoard={setBoard} />
+      </div>
     </div>
   );
 };
