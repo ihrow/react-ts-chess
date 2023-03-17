719f523f940a1ef0efbc2c696a24b788b3bbd1c9
igor <igornotfound@gmail.com>
2023-03-17 23:53:52 +0100

    cell class + component

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,29 @@
+import React, {FC} from 'react';
+import {Cell} from "../models/Cell";
+
+interface CellComponentProps {
+  cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
+}
+
+const CellComponent: FC<CellComponentProps> = ({cell, selected, click}) => {
+  return (
+    <div
+      className={['cell', cell.color, selected ? `selected-${cell.color}` : ''].join(' ')}
+      onClick={() => click(cell)}
+    >
+
+      <span className={['coordinate-x', cell.color, selected ? `selected-${cell.color}` : ''].join(' ')}>
+        {cell.y === 7 ? `${String.fromCharCode(cell.x + 97)}` : null}
+      </span>
+      <span className={['coordinate-y', cell.color, selected ? `selected-${cell.color}` : ''].join(' ')}>
+        {cell.x === 0 ? `${String.fromCharCode(cell.y + 49)}` : null}
+      </span>
+
+      {cell.figure?.icon && <img alt={cell.figure.color + ' ' + cell.figure.name} src={cell.figure.icon} />}
+    </div>
+  );
+};
+
+export default CellComponent;
