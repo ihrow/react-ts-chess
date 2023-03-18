d2bd098d9729134bb82cdfb7f22ab52965b61cee
igor <igornotfound@gmail.com>
2023-03-18 01:34:03 +0100

    moving algorithms + queen moving

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -22,4 +22,62 @@ export class Cell {
     this.id = Math.random()
   }
 
+  public setFigure(figure: Figure) {
+    this.figure = figure;
+    this.figure.cell = this;
+  }
+
+  public moveFigure(target: Cell) {
+    if(this.figure?.canMove(target)) {
+      this.figure?.moveFigure(target)
+      target.setFigure(this.figure)
+      this.figure = null;
+    }
+  }
+
+  public isEmpty(): boolean {
+    return !!this.figure;
+  }
+
+  public isEmptyVertical(target: Cell): boolean {
+    if(this.x !== target.x) return false;
+
+    const min = Math.min(this.y, target.y)
+    const max = Math.max(this.y, target.y)
+    for (let y = min + 1; y < max; y++) {
+      if (this.board.getCell(this.x, y).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public isEmptyHorizontal(target: Cell): boolean {
+    if(this.y !== target.y) return false;
+
+    const min = Math.min(this.x, target.x)
+    const max = Math.max(this.x, target.x)
+    for (let x = min + 1; x < max; x++) {
+      if (this.board.getCell(x, this.y).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public isEmptyDiagonal(target: Cell): boolean {
+    const absX = Math.abs(target.x - this.x);
+    const absY = Math.abs(target.y - this.y);
+    if (absY !== absX) return false;
+
+    const dy = this.y < target.y ? 1 : -1;
+    const dx = this.x < target.x ? 1 : -1;
+
+    for (let i = 1; i < absX; i++) {
+      if (this.board.getCell(this.x + i * dx, this.y + i * dy).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -9,8 +9,16 @@ export class Queen extends Figure {
 
   constructor(color: Colors, cell: Cell) {
     super(color, cell);
-    this.name = FigureNames.BISHOP;
+    this.name = FigureNames.QUEEN;
     this.icon = color === Colors.DARK ? blackQueen : whiteQueen;
+  }
+
+  canMove(target: Cell): boolean {
+    if(!super.canMove(target)) return false;
+    if(this.cell.isEmptyVertical(target)) return true;
+    if(this.cell.isEmptyHorizontal(target)) return true;
+    if(this.cell.isEmptyDiagonal(target)) return true;
+    return false;
   }
 
 }
