a2e5c02e81bf0daa74c718d04000b231afb86588
igor <igornotfound@gmail.com>
2023-03-18 02:03:13 +0100

    pawn move implemented

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -7,10 +7,37 @@ import whitePawn from '../../assets/whitePawn.png'
 
 export class Pawn extends Figure {
 
+  private isFirstStep: boolean = true;
+
   constructor(color: Colors, cell: Cell) {
     super(color, cell);
-    this.name = FigureNames.BISHOP;
+    this.name = FigureNames.PAWN;
     this.icon = color === Colors.DARK ? blackPawn : whitePawn;
+    this.isFirstStep = true;
+  }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) return false;
+    const direction = this.cell.figure?.color === Colors.DARK ? 1 : -1;
+    const firstStepDirection = this.cell.figure?.color === Colors.DARK ? 2 : -2;
+    if ((target.y === this.cell.y + direction || (this.isFirstStep && target.y === this.cell.y + firstStepDirection))
+      && target.x === this.cell.x
+      && this.cell.board.getCell(target.x, target.y).isEmpty()) {
+      return true;
+    }
+
+    if (target.y === this.cell.y + direction
+      && (target.x === this.cell.x + 1 || target.x === this.cell.x - 1)
+      && this.cell.isEnemy(target)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  moveFigure(target: Cell) {
+    super.moveFigure(target);
+    this.isFirstStep = false;
   }
 
 }
