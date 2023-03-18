6813466162bd976392d9048d230d7c2a9a157d25
igor <igornotfound@gmail.com>
2023-03-18 02:22:49 +0100

    king move implemented

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -9,8 +9,16 @@ export class King extends Figure {
 
   constructor(color: Colors, cell: Cell) {
     super(color, cell);
-    this.name = FigureNames.BISHOP;
+    this.name = FigureNames.KING;
     this.icon = color === Colors.DARK ? blackKing : whiteKing;
+  }
+
+  canMove(target: Cell): boolean {
+    if(!super.canMove(target)) return false;
+    const dx = Math.abs(this.cell.x - target.x);
+    const dy = Math.abs(this.cell.y - target.y);
+
+    return (dx === 1 && dy === 1) || (dx === 1 && dy === 0) || (dx === 0 && dy === 1)
   }
 
 }
