6c875ecff7ac775afb227b1de96ccbaf868de242
igor <igornotfound@gmail.com>
2023-03-18 02:20:57 +0100

    en passant implemented

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -6,14 +6,14 @@ import whitePawn from '../../assets/whitePawn.png'
 
 
 export class Pawn extends Figure {
-
-  private isFirstStep: boolean = true;
+  enPassant: boolean;
 
   constructor(color: Colors, cell: Cell) {
     super(color, cell);
     this.name = FigureNames.PAWN;
     this.icon = color === Colors.DARK ? blackPawn : whitePawn;
     this.isFirstStep = true;
+    this.enPassant = false;
   }
 
   canMove(target: Cell): boolean {
@@ -32,11 +32,27 @@ export class Pawn extends Figure {
       return true;
     }
 
+    // en passant
+
+    if (target.y === this.cell.y + direction
+      && (target.x === this.cell.x + 1 || target.x === this.cell.x - 1)
+      && this.cell.board.getCell(target.x, target.y).isEmpty()
+      && this.cell.board.getCell(target.x, this.cell.y).figure?.name === FigureNames.PAWN
+      && this.cell.board.getCell(target.x, this.cell.y).figure?.isFirstStep === false) {
+
+      this.enPassant = true;
+      return true;
+    }
+
+
     return false;
   }
 
   moveFigure(target: Cell) {
     super.moveFigure(target);
+    if (this.enPassant) {
+      this.cell.board.getCell(target.x, this.cell.y).figure = null;
+    }
     this.isFirstStep = false;
   }
 
